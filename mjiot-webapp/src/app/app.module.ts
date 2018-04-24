d154412bd3c4c18e75b733d770b3f295826f7575
Marcin Jahn <jahnmarcin@outlook.com>
2018-04-24 14:02:14 +0200

    Created AuthGuard and UserService. Added basic routes with AuthGuard Added simple AuthGuard logic. Now UserService needs to really check is user is logged in.

--- mjiot-webapp/src/app/app.module.ts
+++ mjiot-webapp/src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
+import {SuiModule} from 'ng2-semantic-ui';
 
+import { AuthGuard } from './auth.guard';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { UserService } from './user.service';
 
-import {SuiModule} from 'ng2-semantic-ui';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  {path: '**', component: MainComponent}
+ ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    SuiModule
+    SuiModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ mjiot-webapp/src/app/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { UserService } from './user.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor( private userService: UserService, private router: Router ) {
+
+  }
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+    ): Observable<boolean> | Promise<boolean> | boolean {
+      console.log('AuthGuard');
+    if (this.userService.isLoggedId())
+      return true;
+    else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
